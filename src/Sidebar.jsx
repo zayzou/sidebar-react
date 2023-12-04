29eaf015e4075363c7697f8f65b708289cc2aeb1
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-12-04 13:08:38 -0500

    checkpoint

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import { useGlobalContext } from './GlobalContext';
-
+import React from "react";
+import { useGlobalContext } from "./GlobalContext";
+import { logo } from "./logo.svg";
+import { soical, links } from "./data";
 const Sidebar = () => {
   // console.log(useGlobalContext().openSidebar);
-  return (
+  return <div>Sidebar</div>;
+};
 
-    <div>Sidebar</div>
-  )
-}
-
-export default Sidebar
+export default Sidebar;
